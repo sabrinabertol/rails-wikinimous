3e02bce358eb1e2b1e6e0318c9ee0126e76cfe41
Sabrina Bertol <bertol.sabrina@gmail.com>
2020-11-04 00:19:17 +0000

    Finished without style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "destroying all articles"
+Article.destroy_all
+puts "Creating new articles..."
+10.times do
+  Article.create!(
+    title: Faker::Cannabis.brand,
+    content: Faker::Lorem.paragraph
+    )
+end
+puts "Done ☺️"
